649232f00188c8224a59d194309b2d54bddf4f97
Slavik F <58614901+zlavik@users.noreply.github.com>
2024-11-12 11:23:42 -0800

    fixed port not working in build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 10000,
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 10000,
+    strictPort: true
   },
 })
